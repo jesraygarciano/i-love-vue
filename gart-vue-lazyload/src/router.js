bc9cb16d6a8ff400fd2047990c9a5d939f035a70
Jesray Garciano <jesray.garciano@gmail.com>
2018-08-17 13:31:24 +0800

    Minor updates

--- gart-vue-lazyload/src/router.js
+++ gart-vue-lazyload/src/router.js
@@ -1,26 +1,23 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
 
-Vue.use(Router);
+Vue.use(Router)
+
+function loadView(view) {
+  return () => import(`@/views/${view}.vue`)
+}
 
 export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
       name: "home",
-      component: Home
+      component: loadView("Home")
     },
     {
-      path: "/about",
+      path: "/",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: loadView("About")
     }
   ]
 });
