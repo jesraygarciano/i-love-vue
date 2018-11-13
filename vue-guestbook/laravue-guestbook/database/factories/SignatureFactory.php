535d30768861761301fed2afbc7348a6c461a4b3
Jesray Garciano <jesray.garciano@gmail.com>
2018-11-13 17:08:27 +0800

    Factory added for generating dummy data

--- vue-guestbook/laravue-guestbook/database/factories/SignatureFactory.php
+++ vue-guestbook/laravue-guestbook/database/factories/SignatureFactory.php
@@ -2,8 +2,10 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
+    $factory->define(App\Signature::class, function (Faker $faker) {
+        return [
+            'name' => $faker->name,
+            'email' => $faker->safeEmail,
+            'body' => $faker->sentence
+        ];
+    });
