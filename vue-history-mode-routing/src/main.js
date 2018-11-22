b8274dd9efc6fe49def77da1734043061eb46109
Jesray Garciano <jesray.garciano@gmail.com>
2018-11-22 09:59:42 +0800

    Importing Components to main js file

--- vue-history-mode-routing/src/main.js
+++ vue-history-mode-routing/src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
+
+import HomeComponent from './components/HomeComponent.vue'
+import DashboardComponent from './components/DashboardComponent.vue'
+import PostComponent from './components/PostComponent.vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 Vue.config.productionTip = false
-const routes = [];
+const routes = [
+  {}
+];
 
 const router = new VueRouter({ mode: 'history',
 routes: routes });
