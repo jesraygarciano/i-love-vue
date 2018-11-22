5f0c7a406443c022cf4eba26babde9fc077faba2
Jesray Garciano <jesray.garciano@gmail.com>
2018-11-22 09:53:20 +0800

    Added Bootstrap and created view components

--- vue-history-mode-routing/src/App.vue
+++ vue-history-mode-routing/src/App.vue
@@ -7,6 +7,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default {
   name: 'app',
--- vue-history-mode-routing/src/main.js
+++ vue-history-mode-routing/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+const routes = [];
+
+const router = new VueRouter({ mode: 'history',
+routes: routes });
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
