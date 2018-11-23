7f5f3650c69eed00cae508c5d9189e6b65b877ca
Jesray Garciano <jesray.garciano@gmail.com>
2018-11-23 14:50:16 +0800

    Finished up API and VUE

---
+++ vue-axios-data-to-api/vueApp.js
@@ -0,0 +1,13 @@
+const url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,EUR";
+
+const vm = new Vue({
+        el: '#app',
+        data: {
+          results: []
+        },
+        mounted() {
+          axios.get(url).then(response => {
+            this.results = response.data
+          })
+        }
+      });
--- vue-axios-display-data-to-api/src/App.vue
+++ vue-axios-display-data-to-api/src/App.vue
@@ -12,6 +12,9 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data(){
+    
   }
 }
 </script>
