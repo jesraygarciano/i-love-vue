ee1feee9538690532ff727c1d39de0d15b149398
Jesray Garciano <jesray.garciano@gmail.com>
2018-12-11 14:35:28 +0800

    Minor update

--- vue-express-mongo/server/index.js
+++ vue-express-mongo/server/index.js
@@ -13,6 +13,7 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
+// https://www.youtube.com/watch?v=j55fHUJqtyw&t=63s
 
 const port = process.env.PORT || 5000;
 
--- vue-express-mongo/server/routes/api/posts.js
+++ vue-express-mongo/server/routes/api/posts.js
@@ -1,1 +1,29 @@
-const express = require('express')
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get posts
+router.get('/', async (req, res) => {   
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}.toArray()));    
+});
+
+
+// Add posts
+
+
+// Delete Posts
+
+
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect
+    ('mongodb://Gart:password1@ds263791.mlab.com:63791/vuemongo', {
+        useNewUrlParser: true
+    });
+
+    return client.db('vuemongo').collection('posts');
+}
+
+module.exports = router;
