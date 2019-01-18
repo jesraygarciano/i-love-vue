297ee1e5c0aad3eaa1a9edbd811af4a67bd98791
Jesray Garciano <jesray.garciano@gmail.com>
2019-01-18 16:14:01 +0800

    Created a new view pages Feature, Grid and Page, Teaser

---
+++ cebu-news/components/Grid.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="util__flex" v-editable="blok">
+        <!-- <component :is="blok.component" :blok="blok" :key="blok._uid" v-for="blok in blok.columns"></component> -->
+        <component :key="blok._uid" v-for="blok in blok.columns" :blok="blok" :is="blok.component"></component>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['blok']
+}
+</script>
